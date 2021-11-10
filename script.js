8f4db982b443999c4ce064c8ff92c6afe277af2d
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-11-10 20:51:56 +0300

    final fix

--- script.js
+++ script.js
@@ -3,13 +3,9 @@
 function showVerticalMessage(str) {
 	if (str.startsWith('м')) {
 		str = 'M' + str.slice(1);
-		for (let char of str.slice(0, 10)) {
-			console.log(char);
-		};
-	} else {
-		for (let char of str) {
-			console.log(char);
-		};
+	};
+	for (let char of str.slice(0, 10)) {
+		console.log(char);
 	};
 };
 
